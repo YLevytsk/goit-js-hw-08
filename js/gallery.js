077ab3c3e98f9f6c0e6e66423a7fbbbb063205d5
yLevytsk <Lupin773311asd@gmail.com>
2025-01-23 12:21:04 +0000

    gdg

--- js/gallery.js
+++ js/gallery.js
@@ -67,28 +67,3 @@ const galleryItems = images.map(image => {
 gallery.append(...galleryItems);
 
 
-const style = document.createElement('style');
-document.head.appendChild(style);
-style.textContent = `
-  .gallery {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    padding: 100px 156px;
-    width: 1440px;
-    height: 848px;
-    list-style-type: none;
-    margin: 0;
-  }
-
-  .gallery-item {
-     width: calc(33.33% - 24px);
-     margin-bottom: 48px;
-  }
-
-  .gallery-item img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-`;
